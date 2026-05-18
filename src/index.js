847ec8d595595ab6aa3ef7e9f4742db59e10a974
Matheus Brayer <mbrayer3@email.com>
2026-05-18 15:01:45 -0300

    Criação do servidor express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hello, World!');
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());    
+
+
+const porta = process.env.PORT;
+app.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`);
+});
+
+
